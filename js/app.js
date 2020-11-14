5d2a6b07d6fb4158bc5a7ada3b38072abb1743a4
mario ciari <mario.ciari@yahoo.fr>
2020-11-14 21:48:52 +0100

    display meteo city

--- js/app.js
+++ js/app.js
@@ -1,14 +1,23 @@
 const btn = document.getElementById('btn-submit')
 const token = '19300c70cce6303be664df99820cbf68'
 const content = document.getElementById('content')
+const divCity = document.getElementById("div-city");
+const divTemp = document.getElementById("div-temp");
+const divSky = document.getElementById("div-sky");
+const divCountry = document.getElementById("div-country");
+
 
 const displayMeteo = (data)=>{
-    const city = document.getElementById('city').value
-    const temp = data.main.temp
-    const weather = data.weather[0].description
-    const country = data.sys.country
-    content.innerHTML = `Le temps est ${weather} et la tempéraure est actuellement de ${temp} degré(s) à ${city} qui se trouve en ${country}`
-    content.innerHTML += '</br>salut je suis Sofia'
+  const city = document.getElementById("city").value;
+  const temp = data.main.temp;
+  const weather = data.weather[0].description;
+  const country = data.sys.country;
+
+  divCity.innerHTML = `<h2>${city}</h2>`;
+  divTemp.innerHTML = `<h2>${temp}°</h2>`;
+  divSky.innerHTML = `<h2>${weather}</h2>`;
+  divCountry.innerHTML = `<h2>${country}</h2>`;
+
 }
 
 btn.addEventListener('click', ()=>{
