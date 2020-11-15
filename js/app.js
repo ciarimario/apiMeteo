f96822ccdbaa8fc2be0e895dcbfbdc1d84beb2b5
mario ciari <mario.ciari@yahoo.fr>
2020-11-15 15:13:22 +0100

    vsion responsive

--- js/app.js
+++ js/app.js
@@ -1,30 +1,54 @@
+// import your Api Key from tokenApiOpenWeather.js
+import {apiKey} from './tokenApiOpenWeather.js'
+
+// select Elements from DOM
 const btn = document.getElementById('btn-submit')
-const token = '19300c70cce6303be664df99820cbf68'
 const content = document.getElementById('content')
 const divCity = document.getElementById("div-city");
 const divTemp = document.getElementById("div-temp");
 const divSky = document.getElementById("div-sky");
 const divCountry = document.getElementById("div-country");
 
+// Constant City By Default
+const defaultCity = "aubervilliers";
 
-const displayMeteo = (data)=>{
-  const city = document.getElementById("city").value;
-  const temp = data.main.temp;
-  const weather = data.weather[0].description;
-  const country = data.sys.country;
-
-  divCity.innerHTML = `<h2>${city}</h2>`;
-  divTemp.innerHTML = `<h2>${temp}°</h2>`;
-  divSky.innerHTML = `<h2>${weather}</h2>`;
-  divCountry.innerHTML = `<h2>${country}</h2>`;
-
+// function Display Meteo Informations
+function displayMeteo(data) {
+    
+    const city = document.getElementById("city").value !="" ? document.getElementById("city").value : defaultCity;
+    // Temperature 
+    const temp = data.main.temp;
+    // Weather Info
+    const weather = data.weather[0].description;
+    // Country Info
+    const country = data.sys.country;
+    
+    // Insert Info in Elements of the Dom
+    divCity.innerHTML = `<h2>${city}</h2>`;
+    divTemp.innerHTML = `<h2>${temp}°</h2>`;
+    divSky.innerHTML = `<h2>${weather}</h2>`;
+    divCountry.innerHTML = `<h2>${country}</h2>`;
 }
 
+// Display Meteo Informations from Default City
+getMeteo()
+
+// 
 btn.addEventListener('click', ()=>{
     const inputCity = document.getElementById('city').value
-    const url =`http://api.openweathermap.org/data/2.5/weather?q=${inputCity}&units=metric&lang=fr&appid=${token}`
+    getMeteo(inputCity)
+} )
+
+// function getMeteo
+// Get Meteo Information from InputCity or default city
+function getMeteo(inputCity= defaultCity) {
+    
+    // Api openWeather
+    const url =`http://api.openweathermap.org/data/2.5/weather?q=${inputCity}&units=metric&lang=fr&appid=${apiKey}`
+    
+    // Ajax request
     fetch(url)
     .then(response=>response.json())
     .then(displayMeteo)
-} )
+}
 
